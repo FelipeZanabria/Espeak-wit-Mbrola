a66b8f3c164ef0f87647c2876ca2e45043367d1b
FelipeZanabria <84168997+FelipeZanabria@users.noreply.github.com>
2023-10-28 04:02:37 -0300

    Update buildVars.py

--- buildVars.py
+++ buildVars.py
@@ -36,15 +36,15 @@ It can span multiple lines."""),
 	# Documentation file name
 	"addon_docFileName": "readme.html",
 	# Minimum NVDA version supported (e.g. "2018.3.0", minor version is optional)
-	"addon_minimumNVDAVersion": "2019.3,
+	"addon_minimumNVDAVersion": "2019.3",
 	# Last NVDA version supported/tested (e.g. "2018.4.0", ideally more recent than minimum version)
-	"addon_lastTestedNVDAVersion": "2023.2,
+	"addon_lastTestedNVDAVersion": "2023.2",
 	# Add-on update channel (default is None, denoting stable releases,
 	# and for development releases, use "dev".)
 	# Do not change unless you know what you are doing!
 	"addon_updateChannel": None,
 	# Add-on license such as GPL 2
-	"addon_license": GPL v2,
+	"addon_license": "GPL v2",
 	# URL for the license document the ad-on is licensed under
 	"addon_licenseURL": "https://www.gnu.org/licenses/gpl-2.0.html",
 }
@@ -57,7 +57,7 @@ It can span multiple lines."""),
 # pythonSources = ["addon/globalPlugins/*.py"]
 # For more information on SCons Glob expressions please take a look at:
 # https://scons.org/doc/production/HTML/scons-user/apd.html
-pythonSources = []"addon/synthDrivers/*.py"
+pythonSources = ["addon/synthDrivers/*.py"]
 
 # Files that contain strings for translation. Usually your python sources
 i18nSources = pythonSources + ["buildVars.py"]

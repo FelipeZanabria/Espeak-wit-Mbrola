d17d2740d97403638874067520cf11e9b7e6faa0
FelipeZanabria <84168997+FelipeZanabria@users.noreply.github.com>
2023-10-29 16:50:32 -0300

    Update buildVars.pyTry fix version number to include in addon store

--- buildVars.py
+++ buildVars.py
@@ -25,7 +25,7 @@ addon_info = {
 	# Translators: Long description to be shown for this add-on on add-on information from add-ons manager
 	"addon_description": _("""This is the old Espeak with Mbrola support ported in an addon for NVDA."""),
 	# version
-	"addon_version": "1.0",
+	"addon_version": "1.0.0",
 	# Author(s)
 	"addon_author": "Felipe Porciuncula Zanabria <felipepzanabria@gmail.com.com>",
 	# URL for the add-on documentation support

a967ed99f0263abd7975083c8bbc326334eda6c6
FelipeZanabria <84168997+FelipeZanabria@users.noreply.github.com>
2024-05-31 02:06:10 -0300

    Update buildVars.py to v1.0.3 and inclute installTasks.py

--- buildVars.py
+++ buildVars.py
@@ -25,7 +25,7 @@ addon_info = {
 	# Translators: Long description to be shown for this add-on on add-on information from add-ons manager
 	"addon_description": _("""This is the old Espeak with Mbrola support ported in an addon for NVDA."""),
 	# version
-	"addon_version": "1.0.2",
+	"addon_version": "1.0.3",
 	# Author(s)
 	"addon_author": "Felipe Porciuncula Zanabria <felipepzanabria@gmail.com>",
 	# URL for the add-on documentation support
@@ -37,7 +37,7 @@ addon_info = {
 	# Minimum NVDA version supported (e.g. "2018.3.0", minor version is optional)
 	"addon_minimumNVDAVersion": "2019.3.0",
 	# Last NVDA version supported/tested (e.g. "2018.4.0", ideally more recent than minimum version)
-	"addon_lastTestedNVDAVersion": "2023.2.0",
+	"addon_lastTestedNVDAVersion": "2023.3.4",
 	# Add-on update channel (default is None, denoting stable releases,
 	# and for development releases, use "dev".)
 	# Do not change unless you know what you are doing!
@@ -56,7 +56,7 @@ addon_info = {
 # pythonSources = ["addon/globalPlugins/*.py"]
 # For more information on SCons Glob expressions please take a look at:
 # https://scons.org/doc/production/HTML/scons-user/apd.html
-pythonSources = ["addon/synthDrivers/*.py"]
+pythonSources = ["addon/*.py", "addon/synthDrivers/*.py"]
 
 # Files that contain strings for translation. Usually your python sources
 i18nSources = pythonSources + ["buildVars.py"]
